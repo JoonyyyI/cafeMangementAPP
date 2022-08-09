dae81cb190ec9d02f7d43391e3f5dcc800440c7c
JoonyyyI <105149840+JoonyyyI@users.noreply.github.com>
2022-08-09 10:10:07 +0900

    Update CafeMenu.java

--- src/model/CafeMenu.java
+++ src/model/CafeMenu.java
@@ -6,12 +6,13 @@
  * @version 1.0
 **/
 package model;
+
 public class CafeMenu {
-    /** 카페 메뉴 종류 **/
-    /** 메뉴 카테코리 **/
-    private String menuCategory;
-    
-    /** 메뉴 이름 **/
+	/** 카페 메뉴 종류 **/
+	/** 메뉴 카테코리 **/
+	private String menuCategory;
+	
+	/** 메뉴 이름 **/
     private String menuName;
     
     /** 메뉴 가격 **/
@@ -23,50 +24,65 @@ public class CafeMenu {
     
     public CafeMenu() {}
     public CafeMenu(String menuCategory, String menuName, int menuPrice, String menuCalorie) {
-        super();
-        this.menuCategory = menuCategory;
-        this.menuName = menuName;
-        this.menuPrice = menuPrice;
-        this.menuCalorie = menuCalorie;
+    	super();
+    	this.menuCategory = menuCategory;
+    	this.menuName = menuName;
+    	this.menuPrice = menuPrice;
+    	this.menuCalorie = menuCalorie;
     }
     
     public String getMenuCategory() {
-        return menuCategory;
+    	return menuCategory;
     }
     
     public void setMenuCategory(String menuCategory) {
-        this.menuCategory = menuCategory;
+    	this.menuCategory = menuCategory;
     }
     
     public String getMenuName() {
-        return menuName;
+    	return menuName;
     }
    
     public void setMenuName(String menuName) {
-        this.menuName = menuName;
+    	this.menuName = menuName;
     }
     
     public int getMenuPrice() {
-        return menuPrice;
+    	return menuPrice;
     }
     
     public void setMenuPrice(int menuPrice) {
-        this.menuPrice = menuPrice;
+    	this.menuPrice = menuPrice;
     }
     
     public String getMenuCalorie() {
-        return menuCalorie;
+    	return menuCalorie;
     }
     
     public void setMenuCalorie(String menuCalorie) {
-        this.menuCalorie = menuCalorie;
+    	this.menuCalorie = menuCalorie;
     }
-   public Object getCafeMenuName() {
-      return null;
-   }
     
-    
-    
+    @Override
+    public String toString() {
+    	StringBuilder builder = new StringBuilder();
+    	builder.append("메뉴 카테고리 : ");
+    	builder.append(menuCategory);
+    	builder.append("\n");
+    	
+        builder.append("메뉴 이름 : ");
+        builder.append(menuName);
+        builder.append("\n");
+        
+     	builder.append("메뉴 가격 : ");
+        builder.append(menuPrice);
+        builder.append("\n");
+        
+        builder.append("메뉴 칼로리 : ");
+        builder.append(menuCalorie);
+        builder.append("\n");
+        return builder.toString();
+    }
     
     
     
